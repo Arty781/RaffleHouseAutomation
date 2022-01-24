f6ed6a83377f737aff44c81c8f2e92d1fa1f4a29
Artem Sukharevsky <sucharevskiy.artem.91@gmail.com>
2022-01-24 19:06:13 +0200

    AddBasketElements

---
+++ PageObjects/Basket/BasketActions.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Allure.Steps;
+using RaffleHouseAutomation.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaffleHouseAutomation.PageObjects
+{
+    public partial class Basket
+    {
+        [AllureStep("Click Add More button")]
+        public Basket ClickAddMoreBtn()
+        {
+            WaitUntil.ElementIsVisibleAndClickable(_addMoreTicketsBtn);
+            addMoreTicketsBtn.Click();
+
+            return this;
+        }
+
+        [AllureStep("Apply coupon Code")]
+        public Basket ApplyCouponCode(string coupon)
+        {
+            WaitUntil.ElementIsVisible(_couponInput);
+            couponInput.SendKeys(coupon);
+            applyCouponBtn.Click();
+
+            return this;
+        }
+
+        [AllureStep("Click Checkout Now button")]
+        public Basket ClickCheckoutNowBtn()
+        {
+            WaitUntil.ElementIsVisible(_checkOutNowBtn);
+            checkOutNowBtn.Click();
+
+            return this;
+        }
+
+        [AllureStep("Open Order summary")]
+        public Basket OpenOrderSummary()
+        {
+            WaitUntil.ElementIsVisible(_orderSummaryBtn);
+            orderSummaryBtn.Click();
+
+            return this;
+        }
+
+        [AllureStep("Enter card details")]
+        public Basket EnterCardDetails()
+        {
+            WaitUntil.ElementIsVisibleAndClickable(_cardNumberInput, 20);
+            cardNumberInput.SendKeys("4242424242424242");
+            WaitUntil.ElementIsVisibleAndClickable(_cardExpDate);
+            cardExpDate.SendKeys("11/28");
+            WaitUntil.ElementIsVisibleAndClickable(_cardCvvInpt);
+            cardCvvInpt.SendKeys("100");
+
+            return this;
+        }
+
+        [AllureStep("Click Pay Now button")]
+        public Basket ClickPayNowBtn()
+        {
+            WaitUntil.ElementIsVisibleAndClickable(_payBtn, 20);
+            payBtn.Click();
+            WaitUntil.WaitSomeInterval(5);
+
+            return this;
+        }
+    }
+}
---
+++ PageObjects/Basket/BasketElements.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using RaffleHouseAutomation.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaffleHouseAutomation.PageObjects
+{
+    public partial class Basket
+    {
+        public IWebElement addMoreTicketsBtn => Browser._Driver.FindElement(_addMoreTicketsBtn);
+        public readonly static By _addMoreTicketsBtn = By.XPath("//*/button[@class='your-basket-ticket-select']");
+        public IWebElement removeOrderBtn => Browser._Driver.FindElement(_removeOrderBtn);
+        public readonly static By _removeOrderBtn = By.XPath("//*/button[@class='basket-btn']");
+        public IWebElement couponInput => Browser._Driver.FindElement(_couponInput);
+        public readonly static By _couponInput = By.XPath("//*/input[@id='coupon-input']");
+        public IWebElement applyCouponBtn => Browser._Driver.FindElement(_applyCouponBtn);
+        public readonly static By _applyCouponBtn = By.XPath("//*/button[@class='apply-coupon-btn']");
+        public IWebElement checkOutNowBtn => Browser._Driver.FindElement(_checkOutNowBtn);
+        public readonly static By _checkOutNowBtn = By.XPath("//*/button[@class='rafflebtn primary full-width']");
+        public IWebElement orderTotalVal => Browser._Driver.FindElement(_orderTotalVal);
+        public readonly static By _orderTotalVal = By.XPath("//*/div[@class='itemPrice total-credit']/div[2]");
+        public IWebElement selectCharityHomless => Browser._Driver.FindElement(_selectCharityHomless);
+        public readonly static By _selectCharityHomless = By.XPath("//*/label[@class='MuiFormControlLabel-root'][1]");
+        public IWebElement selectCharityWomen => Browser._Driver.FindElement(_selectCharityWomen);
+        public readonly static By _selectCharityWomen = By.XPath("//*/label[@class='MuiFormControlLabel-root'][2]");
+        public IWebElement ageControlSection => Browser._Driver.FindElement(_ageControlSection);
+        public readonly static By _ageControlSection = By.XPath("//*/label[@class='MuiFormControlLabel-root'][3]");
+        public IWebElement cardNumberInput => Browser._Driver.FindElement(_cardNumberInput);
+        public readonly static By _cardNumberInput = By.XPath("//*[@id='card-number']");
+        public IWebElement cardExpDate => Browser._Driver.FindElement(_cardExpDate);
+        public readonly static By _cardExpDate = By.XPath("//*[@id='card-expiration-date--description']");
+        public IWebElement cardCvvInpt => Browser._Driver.FindElement(_cardCvvInpt);
+        public readonly static By _cardCvvInpt = By.XPath("//*[@id='card-security-code']");
+        public IWebElement payBtn => Browser._Driver.FindElement(_payBtn);
+        public readonly static By _payBtn = By.XPath("//*/button[@id='pay-button']");
+        public IWebElement orderSummaryBtn => Browser._Driver.FindElement(_orderSummaryBtn);
+        public readonly static By _orderSummaryBtn = By.XPath("//*/div[@class='checkout-header']");
+        
+    }
+}
